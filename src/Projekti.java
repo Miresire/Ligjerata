a71b48d691f29f5acdfb4538079bad0f7018cf92
Miresire <miresirehalimi@gmail.com>
2014-04-02 19:41:56 +0200

    Ushtrimet e para

--- src/Projekti.java
+++ src/Projekti.java
@@ -1,4 +1,16 @@
 
 public class Projekti {
+	public static void main (String []args){
+		int i = 0;
+		char ch = 'a';
+		String s = "String";
+		
+		
+		
+		
+		
+		System.out.println(s);
+	}
+	
 
 }
